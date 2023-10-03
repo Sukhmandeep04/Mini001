6a0d12e8682beabb047126a9b33e14ea0788cb04
Sukhmandeep Kaur <gurmandersingh@Sukhmans-MBP.ht.home>
2023-10-02 21:38:12 -0400

    mini-project-4

--- server.js
+++ server.js
@@ -1,160 +1,97 @@
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
-// Function to read user data from the JSON file
-function readUserData(callback) {
-  fs.readFile(path.join(__dirname, 'users.json'), 'utf8', (err, data) => {
-    if (err) {
-      callback(err);
-    } else {
-      try {
-        const userData = JSON.parse(data);
-        callback(null, userData);
-      } catch (error) {
-        callback(error);
-      }
-    }
-  });
+// Function to read user data from the JSON file using Promises
+function readUserData() {
+  return fs.readFile(path.join(__dirname, 'users.json'), 'utf8')
+    .then(data => JSON.parse(data))
+    .catch(error => {
+      throw error;
+    });
 }
 
-// Function to write user data to the JSON file
-function writeUserData(userData, callback) {
-  fs.writeFile(path.join(__dirname, 'users.json'), JSON.stringify(userData, null, 2), (err) => {
-    if (err) {
-      callback(err);
-    } else {
-      callback(null);
-    }
-  });
+// Function to write user data to the JSON file using Promises
+function writeUserData(userData) {
+  return fs.writeFile(path.join(__dirname, 'users.json'), JSON.stringify(userData, null, 2))
+    .then(() => {
+      return null; // Return null to indicate success without errors
+    })
+    .catch(error => {
+      throw error;
+    });
+}
+
+// Helper function to send responses
+function sendResponse(res, status, contentType, data) {
+  res.writeHead(status, { 'Content-Type': contentType });
+  res.end(data);
 }
 
 // Create an HTTP server
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/api/users') {
-    handleGetUsers(req, res);
-  } else if (req.method === 'POST' && req.url === '/api/users') {
-    handlePostUser(req, res);
-  } else if (req.method === 'PUT' && req.url.startsWith('/api/users/')) {
-    handlePutUser(req, res);
-  } else if (req.method === 'DELETE' && req.url.startsWith('/api/users/')) {
-    handleDeleteUser(req, res);
-  } else {
-    sendErrorResponse(res, 404, 'Not Found');
+const server = http.createServer(async (req, res) => {
+  try {
+    if (req.method === 'GET' && req.url === '/api/users') {
+      const data = await readUserData();
+      sendResponse(res, 200, 'application/json', JSON.stringify(data));
+    } else if (req.method === 'POST' && req.url === '/api/users') {
+      let body = '';
+      req.on('data', (chunk) => {
+        body += chunk;
+      });
+
+      req.on('end', async () => {
+        try {
+          const newUser = JSON.parse(body);
+          const data = await readUserData();
+          data.push(newUser);
+          await writeUserData(data);
+          sendResponse(res, 201, 'text/plain', 'User created successfully');
+        } catch (error) {
+          sendResponse(res, 400, 'text/plain', 'Invalid JSON format');
+        }
+      });
+    } else if (req.method === 'PUT' && req.url.startsWith('/api/users/')) {
+      const userId = req.url.split('/').pop();
+      let body = '';
+      req.on('data', (chunk) => {
+        body += chunk;
+      });
+
+      req.on('end', async () => {
+        try {
+          const updatedUser = JSON.parse(body);
+          const data = await readUserData();
+          const userIndex = data.findIndex((user) => user.id === userId);
+          if (userIndex === -1) {
+            sendResponse(res, 404, 'text/plain', 'User not found');
+          } else {
+            data[userIndex] = { ...data[userIndex], ...updatedUser };
+            await writeUserData(data);
+            sendResponse(res, 200, 'text/plain', 'User updated successfully');
+          }
+        } catch (error) {
+          sendResponse(res, 400, 'text/plain', 'Invalid JSON format');
+        }
+      });
+    } else if (req.method === 'DELETE' && req.url.startsWith('/api/users/')) {
+      const userId = req.url.split('/').pop();
+      const data = await readUserData();
+      const userIndex = data.findIndex((user) => user.id === userId);
+      if (userIndex === -1) {
+        sendResponse(res, 404, 'text/plain', 'User not found');
+      } else {
+        data.splice(userIndex, 1);
+        await writeUserData(data);
+        sendResponse(res, 200, 'text/plain', 'User deleted successfully');
+      }
+    } else {
+      sendResponse(res, 404, 'text/plain', 'Not Found');
+    }
+  } catch (error) {
+    sendResponse(res, 500, 'text/plain', 'Internal Server Error');
   }
 });
-
-function handleGetUsers(req, res) {
-  readUserData((err, data) => {
-    if (err) {
-      sendErrorResponse(res, 500, 'Internal Server Error');
-      return;
-    }
-    sendJSONResponse(res, 200, data);
-  });
-}
-
-function handlePostUser(req, res) {
-  let body = '';
-  req.on('data', (chunk) => {
-    body += chunk;
-  });
-
-  req.on('end', () => {
-    try {
-      const newUser = JSON.parse(body);
-      readUserData((err, data) => {
-        if (err) {
-          sendErrorResponse(res, 500, 'Internal Server Error');
-          return;
-        }
-        data.push(newUser);
-        writeUserData(data, (err) => {
-          if (err) {
-            sendErrorResponse(res, 500, 'Internal Server Error');
-            return;
-          }
-          sendPlainTextResponse(res, 201, 'User created successfully');
-        });
-      });
-    } catch (error) {
-      sendErrorResponse(res, 400, 'Invalid JSON format');
-    }
-  });
-}
-
-function handlePutUser(req, res) {
-  const userId = req.url.split('/').pop();
-  let body = '';
-  req.on('data', (chunk) => {
-    body += chunk;
-  });
-
-  req.on('end', () => {
-    try {
-      const updatedUser = JSON.parse(body);
-      readUserData((err, data) => {
-        if (err) {
-          sendErrorResponse(res, 500, 'Internal Server Error');
-          return;
-        }
-        const userIndex = data.findIndex((user) => user.id === userId);
-        if (userIndex === -1) {
-          sendErrorResponse(res, 404, 'User not found');
-          return;
-        }
-        data[userIndex] = { ...data[userIndex], ...updatedUser };
-        writeUserData(data, (err) => {
-          if (err) {
-            sendErrorResponse(res, 500, 'Internal Server Error');
-            return;
-          }
-          sendPlainTextResponse(res, 200, 'User updated successfully');
-        });
-      });
-    } catch (error) {
-      sendErrorResponse(res, 400, 'Invalid JSON format');
-    }
-  });
-}
-
-function handleDeleteUser(req, res) {
-  const userId = req.url.split('/').pop();
-  readUserData((err, data) => {
-    if (err) {
-      sendErrorResponse(res, 500, 'Internal Server Error');
-      return;
-    }
-    const userIndex = data.findIndex((user) => user.id === userId);
-    if (userIndex === -1) {
-      sendErrorResponse(res, 404, 'User not found');
-      return;
-    }
-    data.splice(userIndex, 1);
-    writeUserData(data, (err) => {
-      if (err) {
-        sendErrorResponse(res, 500, 'Internal Server Error');
-        return;
-      }
-      sendPlainTextResponse(res, 200, 'User deleted successfully');
-    });
-  });
-}
-
-// Helper functions
-function sendJSONResponse(res, status, data) {
-  res.writeHead(status, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify(data));
-}
-
-function sendPlainTextResponse(res, status, message) {
-  res.writeHead(status, { 'Content-Type': 'text/plain' });
-  res.end(message);
-}
-
-function sendErrorResponse(res, status, message) {
-  sendPlainTextResponse(res, status, message);
-}
 
 // Start the server on port 3000
 server.listen(3000, () => {
